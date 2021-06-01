aae58b896ac7d85380f338d1b09acd9ead1f6730
Walker Shaw <walkershaw@foxmail.com>
2021-06-01 22:09:41 +0800

    fix dir issues

--- src/codesign/config.py
+++ src/codesign/config.py
@@ -22,7 +22,7 @@ sw_dir = rst_dir+"./software/"
 
 verbose = False 
 
-
-for path in [rst_dir, rst_dir + model_config_dir, rst_dir + mapping_dir, rst_dir + sw_dir]:
+import os
+for path in [rst_dir, rst_dir + model_config_dir, rst_dir + mapping_dir, sw_dir]:
     if not os.path.exists(path) or not os.path.isdir(path):
         os.mkdir(path)
